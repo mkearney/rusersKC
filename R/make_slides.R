ebf660219cf1045d15b90ed68151beebbd8530e4
Michael Kearney <mkearney@ku.edu>
2017-06-10 15:50:22 -0500

    cleanup R dir

--- R/make_slides.R
+++ R/make_slides.R
@@ -4,11 +4,4 @@ if (!"slidify" %in% installed.packages()) {
     devtools::install_github('ramnathv/slidifyLibraries')
 }
 
-
 slidify::knit2slides("../docs/index.Rmd")
-
-browseURL("../docs/slides.html")
-library(rtweet)
-library(ggplot2)
-
-usrs <- lookup_users(fds$user_id)
